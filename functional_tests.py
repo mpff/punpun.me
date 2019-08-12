526f0b0c8a87a956bfbfe9d9c443653d6cc4170d
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2019-08-12 21:25:21 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,19 +18,38 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Actually finish the damn TDD tutorial" into a text box
         # (Manuels hobby is not finishing coding tutorials)
+        inputbox.send_keys('Actually finish the damn TDD tutorial')
 
         # When he hits enter, the page updates, and now the page lists
-        # "1: Finish the TDD tutorial" as an item in a to-do list
+        # "1: Actually finish the damn TDD tutorial" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Actually finish the damn TDD tutorial' for row
+            in rows
+                )
+            )
 
         # There is stll a text box inviting her to add another item. He
         # enters "Create a website that is not a to-do list" (Manuel loves
         # To-Do lists, but I think he can go a bit further)
+        self.fail('Finish the test!')
 
         # The page updates again, and now it shows both items on his list
 
