434a861e691f329142ec4ed41bde6b79ea8dc7e7
Manuel Pfeuffer <pfeuferm@hu-berlin.de>
2019-08-13 12:03:10 +0200

    Extended Functional Tests. Refactor incoming.

--- functional_tests.py
+++ functional_tests.py
@@ -38,24 +38,32 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Actually finish the damn TDD tutorial' for row
-                in rows),
-            "New to-do item did not appear in table"
-            )
 
         # There is stll a text box inviting her to add another item. He
         # enters "Create a website that is not a to-do list" (Manuel loves
         # To-Do lists, but I think he can go a bit further)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Create a website that is not a to-do list')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
 
         # The page updates again, and now it shows both items on his list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Actually finish the damn TDD tutorial', 
+            [row.text for row in rows]
+            )
+        self.assertIn(
+            '2: Create a website that is not a to-do list', 
+            [row.text for row in rows]
+            )
 
         # Manuel wonders wheter the site will remember her list. Then he
         # sees that the site has generated a unique URL for him -- there is
         # some explanotory text to that effect.
+        self.fail('Finish the test!')
 
         # He visists that URL -- his to-do list is still there.
 
